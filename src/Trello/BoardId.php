9774e985eb81883a664ac1bc16e2565538a4f680
Steve Shanks <steve.c.shanks@gmail.com>
2017-12-30 12:10:58 +0000

    Remove duplication in *Id classes

--- src/Trello/BoardId.php
+++ src/Trello/BoardId.php
@@ -2,25 +2,6 @@
 
 namespace App\Trello;
 
-class BoardId
+class BoardId extends Id
 {
-    /** @var string */
-    protected $id;
-
-    /**
-     * BoardId constructor.
-     * @param string $id
-     */
-    public function __construct(string $id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getId(): string
-    {
-        return $this->id;
-    }
 }
--- src/Trello/ListId.php
+++ src/Trello/ListId.php
@@ -2,25 +2,6 @@
 
 namespace App\Trello;
 
-class ListId
+class ListId extends Id
 {
-    /** @var string */
-    protected $id;
-
-    /**
-     * ListId constructor.
-     * @param string $id
-     */
-    public function __construct(string $id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getId(): string
-    {
-        return $this->id;
-    }
 }

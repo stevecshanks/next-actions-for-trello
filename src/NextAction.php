53b4315cc33d3600d5d07b630c1bcd0e8ae8c588
Steve Shanks <steve.c.shanks@gmail.com>
2018-03-30 20:29:46 +0100

    Add todo list image

--- src/NextAction.php
+++ src/NextAction.php
@@ -70,6 +70,11 @@ class NextAction
         return null;
     }
 
+    public function getImageUrl(): string
+    {
+        return 'https://trello-backgrounds.s3.amazonaws.com/SharedBackground/140x100/28dc90b5fddefcc3dbfdcf6bdc5aa4f6/photo-1484480974693-6ca0a78fb36b.jpg';
+    }
+
     public function isOverdue(): bool
     {
         return $this->getDueDate()->lte(Chronos::now());
